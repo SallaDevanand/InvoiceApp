82518636bb17553b14a15d547393ddf03de48bd7
SallaDevanand <salla.deva70@gmail.com>
2019-03-07 18:10:49 +0400

    Test2

---
+++ src/app/Employee/emploee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emploee',
+  templateUrl: './emploee.component.html',
+  styleUrls: ['./emploee.component.css']
+})
+export class EmploeeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
